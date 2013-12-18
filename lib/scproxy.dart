05b08e24338cd94055a0a90835a6e2ae7a819d1e
darkkiero <paul.vankeuren@gmail.com>
2013-12-17 19:21:17 -0600

    SCsongs are stored in a map.

--- lib/scproxy.dart
+++ lib/scproxy.dart
@@ -1,9 +1,38 @@
 library scproxy;
 import 'dart:js';
+import 'dart:async';
+
+class SCsound {
+  JsObject _proxy;
+  StreamController _state = new StreamController();
+  SCsound(this._proxy){}
+
+  
+  play(){
+    _proxy.callMethod('play');
+  }
+  stop(){
+    _proxy.callMethod('stop');
+  }
+  pause(){
+    _proxy.callMethod('pause');
+  }
+  volume(int value){
+    _proxy.callMethod('setVolume', [value]);
+  }
+  toggleMute(){
+    _proxy.callMethod('toggleMute');
+  }
+  destruct(){
+    _proxy.callMethod('destruct');
+  }
+
+}
 
 class SCproxy {
   String client_id;
   String redirect_uri;
+  Map sounds = new Map();
   
   SCproxy(this.client_id) {
     initialize();
@@ -17,10 +46,21 @@ class SCproxy {
          })         
          ]);
   }
-  stream(String track_id){   
-    context['SC'].callMethod('stream', [track_id, (sound) {
-     sound.callMethod('play');
-   }]);
-  }
   
+  
+  Future <SCsound> load(String name,String track_id){ 
+    var completer = new Completer();
+    
+    context['SC'].callMethod('stream', [track_id, (sound) {
+     sounds[name] = new SCsound(sound);
+     Timer T;
+     T = new Timer.periodic(new Duration(seconds : 1), 
+        (_)  {
+       if (sounds[name] != null)
+        {completer.complete(); T.cancel();}
+        });
+
+   }]);
+    return completer.future; 
+  }
 }
--- test/test.dart
+++ test/test.dart
@@ -1,8 +1,14 @@
 import 'package:scproxy/scproxy.dart';
 
 
-
 main() {
-  SCproxy SC = new SCproxy('your id here');
-  SC.stream('/tracks/111477464'); 
+  SCproxy SC = new SCproxy('72ed25bc5f4c2a06ab8bf3d3e54eef6e');
+  SC.load('firebog','/tracks/111477464').then( (_) => SC.sounds['firebog'].play())
+  .then((_)
+      {
+    SCsound firebog = SC.sounds['firebog'];
+    firebog.volume(10);
+      });
+  
+
 }
